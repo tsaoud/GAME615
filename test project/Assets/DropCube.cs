9dbd68c27ae2d530a4a5fc5fe260c558fe3e403b
Tarek Saoud <ts3076a@student.american.edu>
2021-02-03 16:34:20 -0500

    make cube drop
    added script to make cube drop

---
+++ test project/Assets/DropCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+        }
+    }
+}
